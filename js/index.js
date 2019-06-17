a09ea501864a70fa8d2d3b6fe116b00225727549
Brad <Brad@BRADSPC>
2019-06-17 15:56:35 -0600

    Finished navbar and green text

--- js/index.js
+++ js/index.js
@@ -46,3 +46,23 @@ cta.setAttribute('src', 'img/header-img.png');
 
 let midImg = document.querySelector('#middle-img');
 midImg.setAttribute('src', 'img/mid-page-accent.jpg');
+
+//Fill in JSON data
+//Navbar
+const navBar = document.querySelectorAll('nav a'); 
+navBar[0].textContent = siteContent['nav']['nav-item-1'];
+navBar[1].textContent = siteContent['nav']['nav-item-2'];
+navBar[2].textContent = siteContent['nav']['nav-item-3'];
+navBar[3].textContent = siteContent['nav']['nav-item-4'];
+navBar[4].textContent = siteContent['nav']['nav-item-5'];
+navBar[5].textContent = siteContent['nav']['nav-item-6'];
+
+navBar.forEach(function(aLink){
+  aLink.setAttribute('style', 'color: green');
+});
+
+//Header
+
+//Top Content
+
+//Bottom Content
